ad58f2db4e9661aa0961e1f484a25f9733c1aefb
Shehroz <shehrozehsan@gmail.com>
2020-10-27 20:33:08 +0500

    added all CRUD actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,49 @@
 class ArticlesController < ApplicationController
   
+ 
+
+  def index
+    @articles = Article.all
+  end
+
+  def create
+    @article = Article.new(params.require(:articles).permit(:title, :description))
+    if @article.save
+      flash[:notice] = "Article was saved successfully"
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def show
     @article = Article.find(params[:id])
   end
 
-  def index
-    @articles = Article.all
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was updated successfuly"
+      redirect_to '/articles'
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    if @article.destroy
+      flash[:notice] = "Article was deleted successfully"
+    end
+    redirect_to '/articles'
   end
   
 end

343884ab8d0ff316048ffe0812a574a438a653df
Shehroz <shehrozehsan@gmail.com>
2020-11-09 19:24:49 +0500

    Added User Deletion

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,7 +9,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(params.require(:articles).permit(:title, :description))
-    @article.user = User.first
+    @article.user = current_user
     if @article.save
       flash[:notice] = "Article was saved successfully"
       redirect_to @article
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def index
     @users = User.all
@@ -11,7 +12,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:notice] = "Welcome #{@user.username} to the Alpha Blog, you have signed in successfully"
+      session[:user_id] = @user.id
+      flash[:notice] = "Welcome #{@user.username} to the Alpha Blog, you have signed up successfully"
       redirect_to articles_path
     else
       render 'new'
@@ -19,22 +21,27 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
     @articles = Article.all
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Profile Updations Were Made Successfuly"
       redirect_to user_path(@user)
     else
       render 'edit'
     end
+  end
+
+  def destroy
+    if(@user.destroy)
+      session[:user_id] = nil
+      flash[:notice] = "#{@user.username}'s Account Has Been Removed"
+    end
+    redirect_to articles_path
   end
 
 
@@ -44,5 +51,8 @@ class UsersController < ApplicationController
     params.require(:user).permit(:username, :email, :password)
   end
   
+  def set_user
+    @user = User.find(params[:id])
+  end
 
 end
